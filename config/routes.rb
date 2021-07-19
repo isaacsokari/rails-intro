b573816e679124df5c2496ee6099f4f3370ca03c
Isaac Sokari <xisaac@quidax.com>
2021-07-19 23:47:11 +0100

    setup project and generate home route, add about then create scaffold

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :friends
+	root 'home#index'
+
+	get 'home/about'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ db/migrate/20210719224507_create_friends.rb
@@ -0,0 +1,14 @@
+class CreateFriends < ActiveRecord::Migration[6.1]
+  def change
+    create_table :friends do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.string :twitter
+      t.number :age
+
+      t.timestamps
+    end
+  end
+end
