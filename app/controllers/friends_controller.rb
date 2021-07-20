327af0b7b56a5d5b549ab6a4c41891e5c4417d20
Isaac Sokari <xisaac@quidax.com>
2021-07-20 21:51:57 +0100

    feat: add authorization to perform crud on friends

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,7 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
+	before_action :authenticate_user!, except: [:index, :show]
+	before_action :correct_user, only: [:edit, :update, :destroy]
 
   # GET /friends or /friends.json
   def index
@@ -12,7 +14,7 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+		@friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -21,7 +23,7 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
+		@friend = current_user.friends.build(friend_params)
 
     respond_to do |format|
       if @friend.save
@@ -55,6 +57,11 @@ class FriendsController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+	def correct_user
+		@friend = current_user.friends.find_by(id: params[:id])
+		redirect_to friends_path, notice: "Not Authorized to Edit This Friend" if @friend.nil?
+	end
 
   private
     # Use callbacks to share common setup or constraints between actions.
