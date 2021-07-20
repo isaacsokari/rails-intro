32771a65d1ffc3afdee7fc7e1382f848db0452c9
Isaac Sokari <xisaac@quidax.com>
2021-07-20 21:58:58 +0100

    refactor: use friends view as root with conditional rendering

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :friends
-	root 'home#index'
-
+	
+	# root 'home#index'
+	root 'friends#index'
 	get 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
