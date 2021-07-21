7cbadd8ce438ce0174748cc212fb276edf566616
Isaac Sokari <xisaac@quidax.com>
2021-07-21 11:12:35 +0100

    update about page route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 	
 	# root 'home#index'
 	root 'friends#index'
-	get 'home/about'
+	get '/about', to: 'home#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
